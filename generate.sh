e5461dbf21554d38f8b06a523e402394fd4a9aec
scodx <scodx@melmac.westeros.local>
2020-05-29 15:57:17 +0000

    fixing generate.php

--- generate.sh
+++ generate.sh
@@ -3,9 +3,9 @@
 RANDOM_STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 git reset --hard origin/master
-echo RANDOM_STRING >> activity.txt
+echo $RANDOM_STRING >> activity.txt
 git add .
-git commit -m 'adding RANDOM_STRING to activity.txt';
+git commit -m 'adding $RANDOM_STRING to activity.txt';
 git push origin master;
 
 
